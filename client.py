91dfa0ce349ec36668e851fc435e95c103e6a0fe
rangga <ranggaaldosas@gmail.com>
2023-04-02 08:39:50 +0700

    first commit

---
+++ client.py
@@ -0,0 +1,80 @@
+import grpc
+import todo_pb2
+import todo_pb2_grpc
+import telegram
+from telegram.ext import CommandHandler, Updater, PicklePersistence
+
+# Inisialisasi bot telegram
+bot = telegram.Bot(token='6116776439:AAFsxLB9FF_GMDWNAq3OGx6Qi1cjE3U6SrA')
+updater = Updater(token='6116776439:AAFsxLB9FF_GMDWNAq3OGx6Qi1cjE3U6SrA',
+                  use_context=True)
+dispatcher = updater.dispatcher
+
+
+def start(update, context):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Halo! Gunakan /add <judul todo> untuk menambahkan todo list")
+
+
+def add(update, context):
+    title = context.args[0]
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = todo_pb2_grpc.TodoServiceStub(channel)
+        response = stub.Create(todo_pb2.CreateRequest(title=title))
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text=response.message)
+
+
+def read(update, context):
+    id = context.args[0]
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = todo_pb2_grpc.TodoServiceStub(channel)
+        try:
+            response = stub.Read(todo_pb2.ReadRequest(id=id))
+            context.bot.send_message(chat_id=update.effective_chat.id,
+                                     text=response.title)
+        except grpc.RpcError as e:
+            context.bot.send_message(chat_id=update.effective_chat.id,
+                                     text=str(e))
+
+
+def update(update, context):
+    id = context.args[0]
+    title = context.args[1]
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = todo_pb2_grpc.TodoServiceStub(channel)
+        try:
+            response = stub.Update(todo_pb2.UpdateRequest(id=id, title=title))
+            context.bot.send_message(chat_id=update.effective_chat.id,
+                                     text=response.message)
+        except grpc.RpcError as e:
+            context.bot.send_message(chat_id=update.effective_chat.id,
+                                     text=str(e))
+
+
+def delete(update, context):
+    id = context.args[0]
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = todo_pb2_grpc.TodoServiceStub(channel)
+        try:
+            response = stub.Delete(todo_pb2.DeleteRequest(id=id))
+            context.bot.send_message(chat_id=update.effective_chat.id,
+                                     text=response.message)
+        except grpc.RpcError as e:
+            context.bot.send_message(chat_id=update.effective_chat.id,
+                                     text=str(e))
+
+
+start_handler = CommandHandler('start', start)
+add_handler = CommandHandler('add', add)
+read_handler = CommandHandler('read', read)
+update_handler = CommandHandler('update', update)
+delete_handler = CommandHandler('delete', delete)
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(add_handler)
+dispatcher.add_handler(read_handler)
+dispatcher.add_handler(update_handler)
+dispatcher.add_handler(delete_handler)
+
+updater.start_polling()
---
+++ server.py
@@ -0,0 +1,63 @@
+import grpc
+import firebase_admin
+from firebase_admin import credentials, firestore
+import todo_pb2
+import todo_pb2_grpc
+from concurrent import futures
+
+# Inisialisasi firebase
+cred = credentials.Certificate('./serviceAccountKey.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+
+class TodoService(todo_pb2_grpc.TodoServiceServicer):
+
+    def Create(self, request, context):
+        todo_ref = db.collection('todo').document()
+        todo_ref.set({'title': request.title})
+        return todo_pb2.CreateResponse(message='Todo created')
+
+    def Read(self, request, context):
+        todo_ref = db.collection('todo').document(request.id)
+        todo = todo_ref.get()
+        if todo.exists:
+            return todo_pb2.ReadResponse(title=todo.to_dict()['title'])
+        else:
+            context.set_details('Todo not found')
+            context.set_code(grpc.StatusCode.NOT_FOUND)
+            return todo_pb2.ReadResponse()
+
+    def Update(self, request, context):
+        todo_ref = db.collection('todo').document(request.id)
+        todo = todo_ref.get()
+        if todo.exists:
+            todo_ref.update({'title': request.title})
+            return todo_pb2.UpdateResponse(message='Todo updated')
+        else:
+            context.set_details('Todo not found')
+            context.set_code(grpc.StatusCode.NOT_FOUND)
+            return todo_pb2.UpdateResponse()
+
+    def Delete(self, request, context):
+        todo_ref = db.collection('todo').document(request.id)
+        todo = todo_ref.get()
+        if todo.exists:
+            todo_ref.delete()
+            return todo_pb2.DeleteResponse(message='Todo deleted')
+        else:
+            context.set_details('Todo not found')
+            context.set_code(grpc.StatusCode.NOT_FOUND)
+            return todo_pb2.DeleteResponse()
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    todo_pb2_grpc.add_TodoServiceServicer_to_server(TodoService(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    serve()
